249141e89c08aa5b76b51f9499a0a49f2db02882
Philip Kranz <pk@pmlk.net>
2018-08-18 14:55:59 +0200

    Dont use stack in 'default.nix'

--- default.nix
+++ default.nix
@@ -1,8 +1,16 @@
-{ghc}:
-with (import <nixpkgs> {});
-
-haskell.lib.buildStackProject {
-  inherit ghc;
-  name = "rechord";
-  buildInputs = [ pkgconfig cairo zlib ];
+{ mkDerivation, base, cairo, containers, directory, filepath
+, haScales, lens, mtl, parsec, stdenv, text, time
+}:
+mkDerivation {
+  pname = "rechord";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base cairo containers directory filepath haScales lens mtl parsec
+    text time
+  ];
+  description = "Generate pretty chord sheets from ChordPro-like text files";
+  license = stdenv.lib.licenses.gpl3;
 }
