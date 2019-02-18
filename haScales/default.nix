4d6b78053250a083d68506ba963dda1c4eaa4614
Philip Kranz <pk@pmlk.net>
2019-02-18 17:05:02 +0100

    Screw nix. Use stack. Period.
    We still need stack.nix so that Nix users get
    the necessary dependencies.

--- haScales/default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, containers, stdenv }:
-mkDerivation {
-  pname = "haScales";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base containers ];
-  description = "A music theory library";
-  license = stdenv.lib.licenses.bsd3;
-}
