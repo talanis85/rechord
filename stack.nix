1f57dd9dd4ad8fa919944f4b74d9bb0ecbdd8e43
Philip Kranz <pk@pmlk.net>
2021-10-03 12:37:11 +0200

    Pin nixpkgs to nixos-18.09 so we get the correct ghc version.

--- stack.nix
+++ stack.nix
@@ -1,8 +1,14 @@
 {ghc}:
 with (import <nixpkgs> {});
 
-haskell.lib.buildStackProject {
-  inherit ghc;
-  name = "rechord";
-  buildInputs = [ pkgconfig cairo git zlib ];
-}
+let
+  nixpkgs1809 = import (fetchTarball https://github.com/NixOS/nixpkgs/archive/refs/tags/18.09.tar.gz) {
+    config = config // { allowBroken = true; };
+  };
+
+in
+  nixpkgs1809.haskell.lib.buildStackProject {
+    ghc = nixpkgs1809.haskell.compiler.ghc802;
+    name = "rechord";
+    buildInputs = [ nixpkgs1809.pkgconfig nixpkgs1809.cairo nixpkgs1809.git nixpkgs1809.zlib ];
+  }
